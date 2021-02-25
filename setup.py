d50daecac40d8bf2af81cb1d78aac1bec66cd01e
M. Mert Yildiran <mehmet@up9.com>
2021-02-25 12:59:10 +0300

    Bump the version to 0.3.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="gopygo",
     packages=["gopygo"],
-    version="0.2.0",
+    version="0.3.0",
     author="M. Mert Yildiran",
     author_email="mehmet@up9.com",
     url="http://github.com/up9inc/gopygo",
